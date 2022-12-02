47247bbb5e5353b4a14f5627a4163b603d7b4001
MadeIndri07 <madeindrim@gmail.com>
2022-12-03 01:55:39 +0700

    landing page & leaderboard v1

--- lib/widget/Drawer.dart
+++ lib/widget/Drawer.dart
@@ -5,6 +5,9 @@ import 'package:wazzt/main.dart';
 import '../modules/account_auth/pages/login_page.dart';
 import '../modules/account_auth/utils/cookie_request.dart';
 import '../modules/account_auth/utils/logout.dart';
+import '../modules/leaderboard/leaderboard.dart';
+import '../modules/profile/showProfilePage.dart';
+
 
 Drawer buildDrawer(BuildContext context) {
   final _request = Provider.of<CookieRequest>(context, listen: false);
@@ -34,6 +37,43 @@ Drawer buildDrawer(BuildContext context) {
                     builder: (context) => LoginPage(),
                   ));
             }),
+        ListTile(
+            title: const Text('Wazzt'),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage(title: ""),
+              ));
+            }
+        ),
+        ListTile(
+          title: const Text('Forum'),
+          //     onTap: () {
+          //       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MyFormPage()),);
+          //     }
+        ),
+        ListTile(
+          title: const Text('Profile'),
+              onTap: () {
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const ShowProfilePage()),);
+              }
+        ),
+        ListTile(
+            title: const Text('Leaderboard'),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const LeaderboardPage()),);
+            }
+        ),
+        ListTile(
+          title: const Text('Tukar Poin'),
+          //     onTap: () {
+          //       Navigator.push(context, MaterialPageRoute(builder: (context) => const WatchListPage()),);
+          //     }
+        ),
+        ListTile(
+          title: const Text('Riwayat'),
+          //     onTap: () {
+          //       Navigator.push(context, MaterialPageRoute(builder: (context) => const WatchListPage()),);
+          //     }
+        ),
       ],
     ),
   );
