2c2177893a17702d7b498bf8783225817483636b
farizedaandhika21 <farizedaandhika21@gmail.com>
2022-12-07 23:24:38 +0700

    History done

--- lib/modules/sumbangan/utils/fetch.dart
+++ lib/modules/sumbangan/utils/fetch.dart
@@ -3,30 +3,33 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Future<List<Donasi>> fetchHistory(int id) async {
-  var url = Uri.parse('https://wazzt.up.railway.app/sumbang/history/' + id.toString());
+  var url = Uri.parse('https://wazzt.up.railway.app/sumbang/history/' + id.toString() + "/") ;
   print(url);
   var response = await http.get(
     url,
     headers: {
-      "Access-Control-Allow-Origin": "*",
       "Content-Type": "application/json",
     },
   );
 
   var data = jsonDecode(utf8.decode(response.bodyBytes));
-  print("abis ini data");
+  print("abis ini data:");
   print(data);
 
   // melakukan konversi data json menjadi object Donasi
   List<Donasi> daftarDonasi = [];
+  print("ini daftar donasi");
+  print(daftarDonasi);
 
   for (var d in data) {
     if (d != null) {
+      print("ini d:");
       print(d);
       daftarDonasi.add(Donasi.fromJson(d));
-
     }
   }
+
+  print("abis for loop print daftar donasi");
   print(daftarDonasi);
   return daftarDonasi;
 }
